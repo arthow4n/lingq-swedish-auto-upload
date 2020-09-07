33a76fb0ba0f1aafd28117775475dc9f1fc2c031
arthow4n <13860646+arthow4n@users.noreply.github.com>
2020-09-07 18:00:36 +0200

    Add auto table cleanup for running on Heroku

--- src/db/importedUrl.ts
+++ src/db/importedUrl.ts
@@ -4,6 +4,20 @@ import { without } from "lodash";
 const prisma = new PrismaClient();
 
 export const markAsImported = async (url: string) => {
+  const count = await prisma.importedUrls.count();
+
+  // Heroku free plan only offer 10000 rows of storage
+  // https://elements.heroku.com/addons/heroku-postgresql
+  if (count > 9000) {
+    await prisma.importedUrls.deleteMany({
+      where: {
+        date: {
+          lt: new Date(+new Date() - 5184000000),
+        },
+      },
+    });
+  }
+
   await prisma.importedUrls.create({
     data: {
       date: new Date(),
